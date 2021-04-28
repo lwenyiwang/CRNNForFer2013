89570ced01e1a5bd7e6f101f69eb51d3425f89cc
lwenyiwang <83348715+lwenyiwang@users.noreply.github.com>
2021-04-28 21:22:01 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
-'''Train CK+ with PyTorch.'''
-# 10 crop for data enhancement
+
 from __future__ import print_function
-import os
-os.environ["CUDA_VISIBLE_DEVICES"]='3'
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
